9ce88fdf7e0098b7c64c259158ade65b2df7739b
Herp Derpinstine <herpderpinstine@gmail.com>
2021-02-12 03:56:34 -0600

    Cleanup

--- Releases.cs
+++ Releases.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Security.Cryptography;
-using System.Windows.Forms;
+﻿using System.Collections.Generic;
 using MelonLoader.LightJson;
 
 namespace MelonLoader
